2be1d70188334bd61a92b9d7eab92be6f5ffd43e
Uliana Asadulina <u.asadulina@icloud.com>
2025-02-04 00:09:26 +0000

    SearchBar Component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,13 @@
 import "../styles.css";
 import Header from "./Header";
+import SearchBar from "./SearchBar";
+
 function HomePage() {
   return (
     <div style={{ border: "solid orange", width: "50%" }}>
       Home page
       <Header />
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,5 @@
+function SearchBar() {
+  return <div style={{ border: "solid green" }}>search</div>;
+}
+
+export default SearchBar;
